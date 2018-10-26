727f6081ec20c55efbc4605973551cde758f0df3
israelgSp <Israelg@spectralogic.com>
2018-10-26 10:36:20 -0600

    Added one test thus far for shuffling quiz .

--- quiz/quiz_test.go
+++ quiz/quiz_test.go
@@ -1,8 +1,20 @@
 package main
 
-import "testing"
-
+import (
+	"testing"
+	"runtime"
+	"path"
+	"reflect"
+)
 
 func TestShuffle(t *testing.T) {
-
+	_, filename, _, _ := runtime.Caller(0)
+	file := path.Join(path.Dir(filename), "problems.csv")
+	var quiz []Quiz
+	quiz = parseCSVFile(file)
+	shuffledQuiz := shuffleArray(quiz)
+	return_bool := reflect.DeepEqual(quiz, shuffledQuiz)
+	if return_bool == false {
+		t.Errorf("Quiz has not been shuffled")
+	}
 }
