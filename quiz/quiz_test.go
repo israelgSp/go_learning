a2fe56d045423eb117cd80b98f4978d9aa205f76
israelgSp <Israelg@spectralogic.com>
2018-10-26 12:28:02 -0600

    Refactored shuffle test to not use the parseCSV method. Also began to implement the TestParseCSVFile.

--- quiz/quiz_test.go
+++ quiz/quiz_test.go
@@ -2,16 +2,18 @@ package main
 
 import (
 	"testing"
-	"runtime"
-	"path"
 	"reflect"
 )
 
 //Test shuffle quiz method
 func TestShuffle(t *testing.T) {
-	_, filename, _, _ := runtime.Caller(0)
-	file := path.Join(path.Dir(filename), "problems.csv")
-	quiz := parseCSVFile(file)
+	quiz := []Quiz{
+		{"5+5","10"},
+		{"1+1","2"},
+		{"8+3","11"},
+		{"1+2","3"},
+		{"8+6","14"},
+	}
 	shuffledQuiz := shuffleQuiz(quiz)
 	return_bool := reflect.DeepEqual(quiz, shuffledQuiz)
 	
@@ -19,9 +21,14 @@ func TestShuffle(t *testing.T) {
 		t.Errorf("Quiz has not been shuffled")
 	}
 
-	_, filename1, _, _ := runtime.Caller(0)
-	file1 := path.Join(path.Dir(filename1), "random.csv")
-	quiz1 := parseCSVFile(file1)
+	quiz1 := []Quiz{
+		{"Who is the president of the united states?","Trump"},
+		{"What is the captial of USA?","D.C."},
+		{"What is Israel's favorite color?","Green"},
+		{"What is the captial of Colorado?","Denver"},
+		{"What is the professional basketball organization in USA?","NBA"},
+	}
+
 	shuffledQuiz1 := shuffleQuiz(quiz1)
 	return_bool1 := reflect.DeepEqual(quiz1, shuffledQuiz1)
 
@@ -29,4 +36,8 @@ func TestShuffle(t *testing.T) {
 		t.Errorf("Quiz has not been shuffled")
 	}
 
+}
+
+func TestParseCSVFile(t *testing.T) {
+	
 }
