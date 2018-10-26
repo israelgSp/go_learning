5a369246334b5fe9de7cacbdf8a05bcd5a1a7817
israelgSp <Israelg@spectralogic.com>
2018-10-26 10:53:09 -0600

    Updated TestShuffle to reflect new shuffleQuiz method name.

--- quiz/quiz_test.go
+++ quiz/quiz_test.go
@@ -7,12 +7,13 @@ import (
 	"reflect"
 )
 
+//Test shuffle quiz method
 func TestShuffle(t *testing.T) {
 	_, filename, _, _ := runtime.Caller(0)
 	file := path.Join(path.Dir(filename), "problems.csv")
 	var quiz []Quiz
 	quiz = parseCSVFile(file)
-	shuffledQuiz := shuffleArray(quiz)
+	shuffledQuiz := shuffleQuiz(quiz)
 	return_bool := reflect.DeepEqual(quiz, shuffledQuiz)
 	if return_bool == false {
 		t.Errorf("Quiz has not been shuffled")
